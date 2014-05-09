e6622ae5e4fa83b4317bda441c4f163584b2bbb2
Kramer Campbell <kramer@kramerc.com>
2014-05-08 19:18:52 -0700

    Add package task. Fix static path finding.
    Using path.resolve does not work as intended when the packaged OS X app
    bundle is executed directly.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var async = require('async');
 var gulp = require('gulp');
 var mkdirp = require('mkdirp');
+var ncp = require('ncp');
+var path = require('path');
 var spawn = require('child_process').spawn;
 var standaloneGruntRunner = require('standalone-grunt-runner');
 
@@ -11,7 +13,15 @@ var paths = {
   atomShell: {
     downloadDir: 'cache/atom-shell',
     outputDir: 'dep/atom-shell'
-  }
+  },
+  distDir: 'dist',
+  libDir: 'lib',
+  package: 'package.json',
+  resourcesDir: {
+    mac: 'dist/Atom.app/Contents/Resources/app',
+    others: 'dist/atom-shell/resources/app'
+  },
+  staticDir: 'static'
 };
 
 gulp.task('default', ['run']);
@@ -34,6 +44,47 @@ gulp.task('download-atom-shell', function (callback) {
       npm: 'grunt-download-atom-shell'
     }, function () {
       callback();
+    });
+  });
+});
+
+gulp.task('package', ['download-atom-shell'], function (callback) {
+  var copy = function (src, dest) {
+    return gulp.src(src)
+      .pipe(gulp.dest(dest));
+  };
+
+  var copyDir = function (src, dest) {
+    return function (cb) {
+      ncp(src, dest, function (err) {
+        cb(err);
+      });
+    };
+  };
+
+  ncp(paths.atomShell.outputDir, paths.distDir, function (err) {
+    if (err) {
+      return callback(err);
+    }
+
+    var distDir;
+    if (process.platform === 'darwin') {
+      distDir = paths.resourcesDir.mac;
+    } else {
+      distDir = paths.resourcesDir.others;
+    }
+
+    mkdirp(distDir, function (err) {
+      if (err) {
+        return callback(err);
+      }
+
+      copy(paths.package, distDir);
+
+      async.parallel([
+        copyDir(paths.libDir, path.join(distDir, 'lib')),
+        copyDir(paths.staticDir, path.join(distDir, 'static'))
+      ], callback(err));
     });
   });
 });
