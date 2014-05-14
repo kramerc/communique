cda26b7af16ccfc0e18a6becf25735fb5d7da963
Kramer Campbell <kramer@kramerc.com>
2014-05-14 16:46:37 -0700

    Maintain messages scroll position.

--- lib/components/message-list.jsx
+++ lib/components/message-list.jsx
@@ -5,6 +5,17 @@ var React = require('react');
 var Message = require('./message');
 
 var MessageList = React.createClass({
+  componentWillUpdate: function () {
+    var node = this.getDOMNode();
+    this.shouldScrollToBottom =
+      node.scrollTop + node.offsetHeight === node.scrollHeight;
+  },
+  componentDidUpdate: function () {
+    var node = this.getDOMNode();
+    if (this.shouldScrollToBottom) {
+      node.scrollTop = node.scrollHeight;
+    }
+  },
   render: function () {
     var messageNodes = this.props.data.map(function (message) {
       return (
